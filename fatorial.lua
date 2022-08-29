2ce585cfefd8deaa7acfd153a7b14a194e8879b6
vattghern <otavioaugustobrb19@gmail.com>
2022-08-29 00:54:18 -0300

    fatorial

---
+++ fatorial.lua
@@ -0,0 +1,17 @@
+function fatorial(num)
+    if num == 0 or num == 1 then
+        
+        return 1
+    
+    else
+        
+        return num * fatorial(num - 1)
+    end
+end
+
+print("Digite um valor para ser fatorado")
+valor = io.read()
+print("O valor digitado foi " .. valor .. "")
+
+print(fatorial(valor))
+
